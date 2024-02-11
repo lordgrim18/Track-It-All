d8159fb51fabf4199dae3844e882886377ecefc0
lordgrim <jeromjomanthara@gmail.com>
2024-02-11 22:39:20 +0530

    feat: add views to blueprint

--- track_it_all/__init__.py
+++ track_it_all/__init__.py
@@ -5,4 +5,8 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = config('SECRET_KEY')
 
+    from .views import views
+
+    app.register_blueprint(views, url_prefix='/')
+
     return app
