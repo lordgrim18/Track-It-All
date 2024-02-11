05ff3269856e75689e857ace71bed37b6286dfe1
lordgrim <jeromjomanthara@gmail.com>
2024-02-11 22:49:40 +0530

    feat(views): add login and sign up routes

--- track_it_all/views.py
+++ track_it_all/views.py
@@ -5,3 +5,11 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template('home.html')
+
+@views.route('/login')
+def login():
+    return render_template('login.html')
+
+@views.route('/sign-up')
+def register():
+    return render_template('sign_up.html')
