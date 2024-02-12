ac7009eb18d9dd6626f5a92472cc01a11ea577c2
lordgrim <jeromjomanthara@gmail.com>
2024-02-12 08:52:15 +0530

    feat(views): add post method and validation to sign up

--- track_it_all/views.py
+++ track_it_all/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 views = Blueprint('views', __name__)
 
@@ -6,10 +6,26 @@ views = Blueprint('views', __name__)
 def home():
     return render_template('home.html')
 
-@views.route('/login')
+@views.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
-@views.route('/sign-up')
+@views.route('/sign-up', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('first_name')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 2 characters.', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            flash('Account created!', category='success')
     return render_template('sign_up.html')
