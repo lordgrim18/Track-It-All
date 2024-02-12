f3221ea1b3b5fab4d8ea66f37744a753ac6be385
lordgrim <jeromjomanthara@gmail.com>
2024-02-12 19:13:44 +0530

    feat(init): add create database fn

--- track_it_all/__init__.py
+++ track_it_all/__init__.py
@@ -1,6 +1,10 @@
 from decouple import config
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+from .views import views
+from .models import User
 
 db = SQLAlchemy()
 DB_NAME = config('DB_NAME')
@@ -11,8 +15,13 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqllite:///{DB_NAME}'
     db.init_app(app)
 
-    from .views import views
-
     app.register_blueprint(views, url_prefix='/')
 
+    create_database(app)
+
     return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created database!')
