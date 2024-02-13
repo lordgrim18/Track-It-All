9a08fb0899f4afb46b006aaa7d1734617a756c93
lordgrim <jeromjomanthara@gmail.com>
2024-02-13 20:29:45 +0530

    feat(auth): add login function

--- track_it_all/auth.py
+++ track_it_all/auth.py
@@ -8,6 +8,19 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
     return render_template('login.html')
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
