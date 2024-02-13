dda83e2a577b5db4b3b7fe48f0cae4df67adfc3d
lordgrim <jeromjomanthara@gmail.com>
2024-02-13 20:24:46 +0530

    style: add file to initialize database seperate from init

--- track_it_all/__init__.py
+++ track_it_all/__init__.py
@@ -1,26 +1,21 @@
 from decouple import config
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from os import path
 
-db = SQLAlchemy()
+from .database import db
+from .views import views
+from .auth import auth
+
 DB_NAME = config('DB_NAME')
-
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = config('SECRET_KEY')
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    db.init_app(app)
-
-    from .views import views
-    from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User
-
+    db.init_app(app)
     with app.app_context():
         db.create_all()
 
--- track_it_all/auth.py
+++ track_it_all/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from . import db 
+from .database import db
 from .models import User
 
 auth = Blueprint('auth', __name__)
