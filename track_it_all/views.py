29bf1c56fca2c3d8702baf2685d3b5ad1d3da346
lordgrim <jeromjomanthara@gmail.com>
2024-02-13 23:32:26 +0530

    feat(views): add bug add function to home route

--- track_it_all/views.py
+++ track_it_all/views.py
@@ -1,9 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
+
+from .models import Bug
+from .database import db
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+    if request.method == 'POST':
+        bug = request.form.get('bug')
+
+        if len(bug) < 1:
+            pass
+        else:
+            new_bug = Bug(text=bug, user_id=current_user.id)
+            db.session.add(new_bug)
+            db.session.commit()
+            flash('Bug added!', category='success')
+        
+
     return render_template('home.html', user=current_user)
