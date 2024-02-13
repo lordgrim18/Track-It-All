24350cae29ab4e8d6fae397e09aca95b76933dde
lordgrim <jeromjomanthara@gmail.com>
2024-02-13 20:42:16 +0530

    feat(auth): add logout function

--- track_it_all/auth.py
+++ track_it_all/auth.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
 
 from .database import db
 from .models import User
@@ -16,6 +17,7 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, try again.', category='error')
@@ -46,7 +48,14 @@ def register():
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1)) 
             db.session.add(new_user)
             db.session.commit()
+            login_user(user, remember=True)
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
 
     return render_template('sign_up.html')
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
