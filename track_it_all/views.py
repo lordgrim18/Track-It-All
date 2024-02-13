4105d4e535591b7e7bbae3f917b9aafb248fd445
lordgrim <jeromjomanthara@gmail.com>
2024-02-13 20:16:46 +0530

    feat(gitignore): add database

--- track_it_all/views.py
+++ track_it_all/views.py
@@ -1,31 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
     return render_template('home.html')
-
-@views.route('/login', methods=['GET', 'POST'])
-def login():
-    return render_template('login.html')
-
-@views.route('/sign-up', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        firstName = request.form.get('firstName')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
-
-        if len(email) < 4:
-            flash('Email must be greater than 4 characters.', category='error')
-        elif len(firstName) < 2:
-            flash('First name must be greater than 2 characters.', category='error')
-        elif password1 != password2:
-            flash('Passwords do not match.', category='error')
-        elif len(password1) < 7:
-            flash('Password must be at least 7 characters.', category='error')
-        else:
-            flash('Account created!', category='success')
-    return render_template('sign_up.html')
