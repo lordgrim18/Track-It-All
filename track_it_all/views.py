8c1d2150ce323f83fb9b68620a5023839f8e84fb
lordgrim <jeromjomanthara@gmail.com>
2024-02-17 23:25:55 +0530

    perf(templates): use url_for in base

--- track_it_all/views.py
+++ track_it_all/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, url_for
 from flask_login import login_required, current_user
 
 from .models import Bug
