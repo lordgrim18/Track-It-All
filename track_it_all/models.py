3134a4b5424f24006bae6e0dfd87910993143dcf
lordgrim <jeromjomanthara@gmail.com>
2024-02-20 14:03:04 +0530

    perf(models): add unique and nullable components to model fields

--- track_it_all/models.py
+++ track_it_all/models.py
@@ -5,14 +5,20 @@ from sqlalchemy.sql import func
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
-    first_name = db.Column(db.String(150))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    bugs = db.relationship('Bug')
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    first_name = db.Column(db.String(150), nullable=False)
+    date = db.Column(db.DateTime(timezone=True), default=func.now(), nullable=False)
+    bugs = db.relationship('Bug', backref='bug_finder', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.email}', '{self.first_name}')"
 
 class Bug(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(150))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    text = db.Column(db.String(150), nullable=False)
+    date = db.Column(db.DateTime(timezone=True), default=func.now(), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Bug('{self.text}')"
