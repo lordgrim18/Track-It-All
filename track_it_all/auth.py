edba61fd966405b99c02810e3f72a6fda7b84e60
lordgrim <jeromjomanthara@gmail.com>
2024-02-20 14:33:18 +0530

    feat(auth): add account route and template

--- track_it_all/auth.py
+++ track_it_all/auth.py
@@ -52,3 +52,8 @@ def register():
 def logout():
     logout_user()
     return redirect(url_for('auth.login'))
+
+@auth.route('/account')
+@login_required
+def account():
+    return render_template('account.html', user=current_user)
