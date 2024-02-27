b75cb8b339ada6e1c13e322f0bce5ecf75285c1d
lordgrim <jeromjomanthara@gmail.com>
2024-02-28 00:24:04 +0530

    perf(utils): resize images before saving

--- track_it_all/utils.py
+++ track_it_all/utils.py
@@ -1,11 +1,16 @@
 import os
 import secrets
+from PIL import Image
 
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_name = random_hex + f_ext
     picture_path = os.path.join('track_it_all/static/profile_pics', picture_name)
-    form_picture.save(picture_path)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
 
     return picture_name
