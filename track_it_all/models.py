8426022ab9b6e28ac508f59120d0308b76c6a853
lordgrim <jeromjomanthara@gmail.com>
2024-02-28 02:45:35 +0530

    perf(models): change bug tables fields

--- track_it_all/models.py
+++ track_it_all/models.py
@@ -10,14 +10,15 @@ class User(db.Model, UserMixin):
     first_name = db.Column(db.String(150), nullable=False)
     date = db.Column(db.DateTime(timezone=True), default=func.now(), nullable=False)
     image_file = db.Column(db.String(60), nullable=False, default='default.jpg')
-    bugs = db.relationship('Bug', backref='bug_finder', lazy=True)
+    bugs = db.relationship('Bug', backref='bug_adder', lazy=True)
 
     def __repr__(self):
         return f"User('{self.email}', '{self.first_name}')"
 
 class Bug(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(150), nullable=False)
+    title = db.Column(db.String(150), nullable=False)
+    desc = db.Column(db.String(150), nullable=False)
     date = db.Column(db.DateTime(timezone=True), default=func.now(), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
