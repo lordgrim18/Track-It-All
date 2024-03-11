5d3f3deb0c3a574cf5ec2759626a6e4fa3cce495
lordgrim <jeromjomanthara@gmail.com>
2024-03-11 14:36:12 +0530

    refactor(users): organized user routes, forms and utils

---
+++ track_it_all/users/utils.py
@@ -0,0 +1,33 @@
+import os
+import secrets
+from PIL import Image
+from flask import url_for
+from flask_mail import Message
+
+from track_it_all import mail
+
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_name = random_hex + f_ext
+    picture_path = os.path.join('track_it_all/static/profile_pics', picture_name)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_name
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message('Password Reset Request', sender='noreply@demo.com', recipients=[user.email])
+    msg.body = f'''To reset your password, visit the following link:
+{url_for('auth.reset_token', token=token, _external=True)} 
+
+If you did not make this request then simply ignore this email and no changes will be made.
+'''
+
+    # mail = import_module('track_it_all').mail 
+    mail.send(msg)
