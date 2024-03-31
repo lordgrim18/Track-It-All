3bed3e1b260399b31185f4347c84600f6b80eb92
Jerom <jeromjomanthara@gmail.com>
2024-03-31 18:16:30 +0530

    feat: add flask db migrations

--- track_it_all/__init__.py
+++ track_it_all/__init__.py
@@ -3,10 +3,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_migrate import Migrate
 
 from track_it_all.configurations import Configurations
 
 db = SQLAlchemy()
+migrate = Migrate()
+
 login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
@@ -20,6 +23,8 @@ def create_app():
     db.init_app(app)
     with app.app_context():
         db.create_all()
+
+    migrate.init_app(app, db)
 
     # bcrypt.init_app(app)
     login_manager.init_app(app)
