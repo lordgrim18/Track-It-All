0000e871cc7f966e66890964aecc7221b78e20bf
Jerom <jeromjomanthara@gmail.com>
2024-03-31 10:14:05 +0530

    feat(track_it_all): add projects blueprint)

--- track_it_all/__init__.py
+++ track_it_all/__init__.py
@@ -27,11 +27,13 @@ def create_app():
 
     from track_it_all.main.routes import main
     from track_it_all.users.routes import users
+    from track_it_all.projects.routes import projects
     from track_it_all.bugs.routes import bugs
     from track_it_all.errors.handlers import errors
 
     app.register_blueprint(main, url_prefix='/')
     app.register_blueprint(users, url_prefix='/')
+    app.register_blueprint(projects, url_prefix='/')
     app.register_blueprint(bugs, url_prefix='/')
     app.register_blueprint(errors)
 
