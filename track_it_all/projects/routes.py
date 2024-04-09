454f618f25bfd68357fabbf422e1919dfb69fd34
Jerom <jeromjomanthara@gmail.com>
2024-04-10 03:11:39 +0530

    perf: update models to use default uuid for id field

--- track_it_all/projects/routes.py
+++ track_it_all/projects/routes.py
@@ -15,7 +15,6 @@ def add_project():
     form = ProjectForm()
     if form.validate_on_submit():
         project = Project(
-            id=str(uuid.uuid4()),
             name=form.name.data, 
             about=form.about.data, 
             personal=form.personal.data,
@@ -27,7 +26,6 @@ def add_project():
         db.session.commit()
 
         project_user_row = project_user.insert().values(
-            id=str(uuid.uuid4()),
             project_id=project.id, 
             user_id=current_user.id, 
             user_role=Project_Roles.MANAGER.value,
