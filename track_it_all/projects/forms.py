46a98ec57cab43e5b533f42a0085c92d7573e3b1
Jerom <jeromjomanthara@gmail.com>
2024-04-12 20:28:07 +0530

    feat(projects): add ProjectUser form

--- track_it_all/projects/forms.py
+++ track_it_all/projects/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, SubmitField, BooleanField, SelectField
+from wtforms.validators import DataRequired, Email, ValidationError
+
+from track_it_all.projects.utils import Project_Roles
+from track_it_all.models import User
 
 class ProjectForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
@@ -9,3 +12,13 @@ class ProjectForm(FlaskForm):
     group_project = BooleanField('Group')
 
     submit = SubmitField('Add')
+
+class ProjectUserForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    role = SelectField('Role', choices=[(role.value, role.value) for role in Project_Roles], validators=[DataRequired()])
+
+    submit = SubmitField('Add')
+
+    def validate_email(form, field):
+        if not User.query.filter_by(email=field.data).first():
+            raise ValidationError('No user with that email exists! Please ask them to sign up.')
