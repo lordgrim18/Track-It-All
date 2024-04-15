23d78457b5d96a3dea9e194ee2ddd26d61f42c65
Jerom <jeromjomanthara@gmail.com>
2024-04-16 05:21:14 +0530

    feat(tests): add test_client fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from decouple import config
 
 from track_it_all import create_app, db
 
@@ -6,7 +7,9 @@ from track_it_all import create_app, db
 def app():
     app = create_app()
     app.config['TESTING'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('SQLALCHEMY_DATABASE_URI_TEST_MYSQL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['WTF_CSRF_ENABLED'] = False
 
     with app.app_context():
         db.create_all()
@@ -16,3 +19,23 @@ def app():
 @pytest.fixture()
 def client(app):
     return app.test_client()
+
+
+@pytest.fixture(scope="session")
+def test_client():
+    flask_app = create_app()
+    flask_app.app_context().push()
+    flask_app.config['TESTING'] = True
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = config('SQLALCHEMY_DATABASE_URI_TEST_MYSQL')
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    flask_app.config['WTF_CSRF_ENABLED'] = False
+
+    with flask_app.app_context():
+        db.create_all()
+
+    client = flask_app.test_client()
+
+    yield client  
+
+    with flask_app.app_context():
+        db.drop_all()
