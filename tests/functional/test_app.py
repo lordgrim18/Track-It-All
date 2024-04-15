5c9921440a24b64975799b951f291f115e5690f0
Jerom <jeromjomanthara@gmail.com>
2024-04-16 05:26:52 +0530

    feat(tests): add login and logout test

--- tests/functional/test_app.py
+++ tests/functional/test_app.py
@@ -1,7 +1,7 @@
 from flask import url_for
 from track_it_all import db
 from track_it_all.models import User
-from track_it_all.users.forms import RegistrationForm
+from track_it_all.users.forms import RegistrationForm, LoginForm
 
 def test_home(client):
     response = client.get("/")
@@ -26,3 +26,26 @@ def test_registration(test_client, app):
 
         new_user = db.session.query(User).filter_by(email="user1@gmail.com").first()
         assert new_user
+
+def test_login(test_client, app):
+    with app.app_context(), app.test_request_context():
+        response = test_client.get(url_for("users.login"), follow_redirects=True)
+        assert response.status_code == 200
+
+        login_form = LoginForm()
+
+        login_form.email.data = "user1@gmail.com"
+        login_form.password.data = "hahahehe"
+        login_form.remember.data = False
+
+        response = test_client.post(url_for("users.login"), data=login_form.data, follow_redirects=True)
+
+        assert response.status_code == 200
+        assert response.request.path == url_for("main.home")
+
+def test_logout(test_client, app):
+    with app.app_context(), app.test_request_context():
+        response = test_client.get(url_for("users.logout"), follow_redirects=True)
+
+        assert response.status_code == 200
+        assert response.request.path == url_for("users.login")
