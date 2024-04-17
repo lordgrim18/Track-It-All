f10b3856278107d93bacbb13fff0124fa1ee2c49
Jerom <jeromjomanthara@gmail.com>
2024-04-17 22:26:30 +0530

    feat(config): add sql_alchemy track modifications

--- track_it_all/configurations.py
+++ track_it_all/configurations.py
@@ -4,6 +4,7 @@ from decouple import config
 class Configurations:
     SECRET_KEY = config('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = config('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = config('EMAIL_HOST')
     MAIL_PORT = config('EMAIL_PORT')
     MAIL_USE_TLS = config('EMAIL_USE_TLS')
